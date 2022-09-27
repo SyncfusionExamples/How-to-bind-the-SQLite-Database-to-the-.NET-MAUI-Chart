51bf2d60aa05df3c28555d743ec998f4c9824936
Devakumar D <devakumar.dhanapoosanam@syncfusion.com>
2022-09-27 08:47:01 +0530

    added maui chart sqlite sample.

---
+++ Chart_Sqlite_Sample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Chart_Sqlite_Sample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
