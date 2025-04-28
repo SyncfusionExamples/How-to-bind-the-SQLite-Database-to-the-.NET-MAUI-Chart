db405485d93143833a2d86020e2fa623448e6888
Harsha <harshamidadhala.k@syncfusion.com>
2025-04-28 16:40:51 +0530

    project version updated

--- Chart_Sqlite_Sample/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Chart_Sqlite_Sample;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
---
+++ Chart_Sqlite_Sample/Chart_Sqlite_Sample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Chart_Sqlite_Sample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
